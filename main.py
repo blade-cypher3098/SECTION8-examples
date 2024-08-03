fd3a59363692dfc029ff107d4ae7ff47d837cab4
Johnathan <blade-runner90l-eml@outlook.com>
2024-08-03 02:23:38 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+
+            NameError == 1;
+
+            if (NameError > 0) {
+
+                ++NameError;
+}
+
+            else {
+
+                    print(end=("nis", "n:"));
+                    print(*nis + 0);
+
+            /}
+
+            class classmethod (argb*, argv) {
+
+                    print("Is there a snake in my best friend's boote!?!?" + argv='nil');
+                    print("Is there a snake's in my best friends boote?!?! " + argv);
+
+                module async(argb*, n) {
+
+                        ++n;
+
+
+            }} do
+                    while (n > argb*){
+
+                            print(argb*);
+
+                    continue;
+
+            } break;
+
+    }
